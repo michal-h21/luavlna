186a67fcca60a1258e3d5d87c1f680f16374980f
mr.mitch <mr.mitch@vfemail.net>
2014-01-01 13:02:42 +0100

    Prepare to startchanges

--- prevent-single.lua
+++ prevent-single.lua
@@ -8,51 +8,83 @@
 local M = {}
 local utf_match = unicode.utf8.match
 local utf_char  = unicode.utf8.char
-local match_char = function(x) return utf_match(x,"%a") end
+local alpha = string.char(37).."a"
+local match_char = function(x) return utf_match(x,alpha) end
 local match_table = function(x, chars)local chars=chars or {}; return chars[x] end 
 local singlechars = nil-- {a=true,i=true,z=true, v=true, u=true, o = true} 
 local debug = false
+local tex4ht = false
 -- Enable processing only for certain letters
 -- must be table in the {char = true, char2=true} form
 local set_singlechars= function(c)
-	--print("Set single chars lua")
-	--for k,_ in pairs(c) do print(k) end
-	singlechars = c
+  --print("Set single chars lua")
+  --for k,_ in pairs(c) do print(k) end
+  singlechars = c
 end
 
+local debug_tex4ht = function(head,p)
+  --[[ local w = node.new("glyph")
+  w.lang = tex.lang
+  w.font = font.current()
+  w.char = 64
+  ]]
+  --node.remove(head,node.prev(p))
+  local w = node.new("whatsit", "special")
+  w.data = "t4ht=<span style='background-color:red;width:2pt;'> </span>"
+  return w, head
+end
+
+local debug_node = function(head,p)
+  local w
+  if tex4ht then
+    w, head = debug_tex4ht(head,p)
+  else
+    w = node.new("whatsit","pdf_literal")                          
+    w.data = "q 1 0 1 RG 1 0 1 rg 0 0 m 0 5 l 2 5 l 2 0 l b Q"           
+  end
+  node.insert_after(head,head,w)                                       
+  node.insert_after(head,w,p)                                          
+  -- return w
+end
+
+
 local set_debug= function(x)
-	debug = x
+  debug = x
+end
+
+local set_tex4ht = function()
+  tex4ht = true
 end
 
 local prevent_single_letter = function (head)                                   
-	local singlechars = singlechars 
-	local test_fn = singlechars and match_table or match_char
-	local space = true
-	while head do
-		local id = head.id 
-		if id == 10 then 
-			space=true
-		elseif space==true and id == 37 and utf_match(utf_char(head.char), "%a") then -- a letter 
-			if test_fn(utf_char(head.char), singlechars ) and head.next.id == 10 then    -- only if we are at a one letter word
-				local p = node.new("penalty")                                           
-				p.penalty = 10000                                                       
-				if debug then
-					local w = node.new("whatsit","pdf_literal")                          
-					w.data = "q 1 0 1 RG 1 0 1 rg 0 0 m 0 5 l 2 5 l 2 0 l b Q"           
-					node.insert_after(head,head,w)                                       
-					node.insert_after(head,w,p)                                          
-				else
-					node.insert_after(head,head,p) 
-				end
-			end                                                                       
-			space = false
-		end                                                                         
-		head = head.next                                                            
-	end                                                                             return  true
+  local singlechars = singlechars --or {} 
+  local test_fn = singlechars and match_table or match_char
+  local space = true
+  while head do
+    local id = head.id 
+    if id == 10 then 
+      space=true
+    elseif space==true and id == 37 and utf_match(utf_char(head.char), alpha) then -- a letter 
+      local lang = head.lang
+      --local s = singlechars.lang
+      if test_fn(utf_char(head.char), singlechars) and head.next.id == 10 then    -- only if we are at a one letter word
+        local p = node.new("penalty")                                           
+        p.penalty = 10000                                                       
+        if debug then
+          local w = debug_node(head,p)
+        else
+          node.insert_after(head,head,p) 
+        end
+      end                                                                       
+      space = false
+    end                                                                         
+    head = head.next                                                            
+  end                                                                             return  true
 end               
 
 M.preventsingle = prevent_single_letter
 M.singlechars = set_singlechars
+M.set_tex4ht  = set_tex4ht
 M.debug = set_debug
 return M
 
