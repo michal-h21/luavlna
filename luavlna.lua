e630a7733f7f95fe20807dadb245661823a6f045
Michal Hoftich <michal.h21@gmail.com>
2014-04-08 15:57:09 +0200

    Added initials handling

--- luavlna.lua
+++ luavlna.lua
@@ -10,11 +10,16 @@ local utf_match = unicode.utf8.match
 local utf_char  = unicode.utf8.char
 local alpha = string.char(37).."a" -- alpha class, entering 
                                    -- percent char directly caused error
+local alphas = {}
 local match_char = function(x) return utf_match(x,alpha) end
 local match_table = function(x, chars)
-	local chars=chars or {}; return chars[x] 
+	local chars=chars or {} 
+	return chars[x] 
 end 
 local singlechars = {} -- {a=true,i=true,z=true, v=true, u=true, o = true} 
+
+local initials = {}
+
 local debug = false
 local tex4ht = false
 -- Enable processing only for certain letters
@@ -26,6 +31,13 @@ local set_singlechars= function(lang,c)
   -- for k,_ in pairs(c) do print(k) end
   singlechars[lang] = c
 end
+
+local set_initials = function(lang,c)
+	local lang = tonumber(lang)
+	initials[lang]=c
+end
+
+set_initials(16,{["Č"] =  true, F= true, G = true})
 
 local debug_tex4ht = function(head,p)
   --[[ local w = node.new("glyph")
@@ -61,6 +73,57 @@ local set_tex4ht = function()
   tex4ht = true
 end
 
+local insert_space = function(head)
+	local p = node.new("penalty")                                           
+	p.penalty = 10000                                                       
+	local debug = debug or false
+	if debug then
+		local w = debug_node(head,p)
+	else
+		node.insert_after(head,head,p) 
+	end
+	return head
+end
+
+local is_alpha = function(c)
+	local status = alphas[c]
+	if not status then 
+	  status = utf_match(c, alpha)
+		alphas[c] = status
+	end
+	return status
+end
+
+-- find whether letter is uppercase
+local up_table = {}
+local is_uppercase= function(c)
+	if not is_alpha(c) then return false end
+  local status = up_table[c]
+	if status ~= nil then
+		return status
+	end
+	status = unicode.utf8.upper(c) == c
+	up_table[c] = status
+	return status
+end
+
+local init_buffer = ""
+local is_initial = function(c, lang)
+	local allowed_initials = initials[lang] or {}
+	init_buffer = init_buffer .. c
+	if is_uppercase(c) then
+		return allowed_initials[init_buffer]
+	else
+		local status = allowed_initials[init_buffer]
+		if not status then 
+			--print ("Not allowed initials:".. init_buffer)
+		  init_buffer = ""
+		end
+		return status
+	end
+end
+	
+
 local prevent_single_letter = function (head)                                   
   local singlechars = singlechars  -- or {} 
   -- match_char matches all single letters, but this method is abbandoned
@@ -68,13 +131,17 @@ local prevent_single_letter = function (head)
   -- languages are supported
   local test_fn = match_table -- singlechars and match_table or match_char
   local space = true
+	local init = false
   while head do
     local id = head.id 
+		local nextn = head.next
     if id == 10 then 
       space=true
+			init = is_initial " " -- reset initials
     elseif space==true and id == 37 and utf_match(utf_char(head.char), alpha) then -- a letter 
       local lang = head.lang
 			local char = utf_char(head.char)
+			init = is_initial(char,lang)
       local s = singlechars[lang] or {} -- load singlechars for node's lang
 			--[[
       for k, n in pairs(singlechars) do
@@ -83,16 +150,18 @@ local prevent_single_letter = function (head)
         end
       end
 			--]]
-      if test_fn(char, s) and head.next.id == 10 then    -- only if we are at a one letter word
-        local p = node.new("penalty")                                           
-        p.penalty = 10000                                                       
-        if debug then
-          local w = debug_node(head,p)
-        else
-          node.insert_after(head,head,p) 
-        end
+      if test_fn(char, s) and nextn.id == 10 then    -- only if we are at a one letter word
+				head = insert_space(head)
       end                                                                       
       space = false
+			-- handle initials
+			-- uppercase letter followed by period (code 46)
+		elseif init and head.id == 37 and head.char == 46 and nextn.id == 10 then 
+			print ("Iniciála: " ..init_buffer)
+		elseif head.id == 37 then
+			local char = utf_char(head.char)
+			if init then print "Init true" end
+			init = is_initial(char, head.lang)
     end                                                                         
     head = head.next                                                            
   end                                                                             return  true
