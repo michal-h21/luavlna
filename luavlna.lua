fce421d0558a556f4a3b5028abfa23fdff3cf937
Michal Hoftich <michal.h21@gmail.com>
2025-03-23 14:24:49 +0100

    handle italic correction

--- luavlna.lua
+++ luavlna.lua
@@ -12,16 +12,18 @@ local M = {}
 local utf_char  = utf8.char -- unicode.utf8.char
 local utf_len   = utf8.len -- unicode.utf8.len
 
+local node = node or {}
 local glue_id  = node.id "glue"
 local glyph_id = node.id "glyph"
 local hlist_id = node.id "hlist"
 local vlist_id = node.id "vlist"
 local penalty_id = node.id "penalty"
+local kern_id = node.id "kern"
 local math_id  = node.id "math"
 local period_char = string.byte(".")
 
 -- we must process only glues with the spaceskip or xspaceskip subtypes
-local allowed_space_names = {spaceskip = true, xspaceskip = true}
+local allowed_space_names = {spaceskip = true, xspaceskip = true, userskip = true}
 local allowed_space_types = {}
 local glue_subtypes = node.subtypes("glue")
 -- convert allowed subtypes names to numbers used in nodes
@@ -30,6 +32,16 @@ for k,v in pairs(glue_subtypes) do
     allowed_space_types[k] = true
   end
 end
+
+local allowed_kern_names = {italiccorrection=true}
+local allowed_kern_types = {}
+local kern_subtypes = node.subtypes("kern")
+for k,v in pairs(kern_subtypes) do
+  if allowed_kern_names[v] then
+    allowed_kern_types[k] = true
+  end
+end
+
 
 local unicode_alphas = "0041-005A0061-007A00AA00B500BA00C0-00D600D8-00F600F8-02C102C6-02D102E0-02E402EC02EE0370-037403760377037A-037D03860388-038A038C038E-03A103A3-03F503F7-0481048A-05270531-055605590561-058705D0-05EA05F0-05F20620-064A066E066F0671-06D306D506E506E606EE06EF06FA-06FC06FF07100712-072F074D-07A507B107CA-07EA07F407F507FA0800-0815081A082408280840-085808A008A2-08AC0904-0939093D09500958-09610971-09770979-097F0985-098C098F09900993-09A809AA-09B009B209B6-09B909BD09CE09DC09DD09DF-09E109F009F10A05-0A0A0A0F0A100A13-0A280A2A-0A300A320A330A350A360A380A390A59-0A5C0A5E0A72-0A740A85-0A8D0A8F-0A910A93-0AA80AAA-0AB00AB20AB30AB5-0AB90ABD0AD00AE00AE10B05-0B0C0B0F0B100B13-0B280B2A-0B300B320B330B35-0B390B3D0B5C0B5D0B5F-0B610B710B830B85-0B8A0B8E-0B900B92-0B950B990B9A0B9C0B9E0B9F0BA30BA40BA8-0BAA0BAE-0BB90BD00C05-0C0C0C0E-0C100C12-0C280C2A-0C330C35-0C390C3D0C580C590C600C610C85-0C8C0C8E-0C900C92-0CA80CAA-0CB30CB5-0CB90CBD0CDE0CE00CE10CF10CF20D05-0D0C0D0E-0D100D12-0D3A0D3D0D4E0D600D610D7A-0D7F0D85-0D960D9A-0DB10DB3-0DBB0DBD0DC0-0DC60E01-0E300E320E330E40-0E460E810E820E840E870E880E8A0E8D0E94-0E970E99-0E9F0EA1-0EA30EA50EA70EAA0EAB0EAD-0EB00EB20EB30EBD0EC0-0EC40EC60EDC-0EDF0F000F40-0F470F49-0F6C0F88-0F8C1000-102A103F1050-1055105A-105D106110651066106E-10701075-1081108E10A0-10C510C710CD10D0-10FA10FC-1248124A-124D1250-12561258125A-125D1260-1288128A-128D1290-12B012B2-12B512B8-12BE12C012C2-12C512C8-12D612D8-13101312-13151318-135A1380-138F13A0-13F41401-166C166F-167F1681-169A16A0-16EA1700-170C170E-17111720-17311740-17511760-176C176E-17701780-17B317D717DC1820-18771880-18A818AA18B0-18F51900-191C1950-196D1970-19741980-19AB19C1-19C71A00-1A161A20-1A541AA71B05-1B331B45-1B4B1B83-1BA01BAE1BAF1BBA-1BE51C00-1C231C4D-1C4F1C5A-1C7D1CE9-1CEC1CEE-1CF11CF51CF61D00-1DBF1E00-1F151F18-1F1D1F20-1F451F48-1F4D1F50-1F571F591F5B1F5D1F5F-1F7D1F80-1FB41FB6-1FBC1FBE1FC2-1FC41FC6-1FCC1FD0-1FD31FD6-1FDB1FE0-1FEC1FF2-1FF41FF6-1FFC2071207F2090-209C21022107210A-211321152119-211D212421262128212A-212D212F-2139213C-213F2145-2149214E218321842C00-2C2E2C30-2C5E2C60-2CE42CEB-2CEE2CF22CF32D00-2D252D272D2D2D30-2D672D6F2D80-2D962DA0-2DA62DA8-2DAE2DB0-2DB62DB8-2DBE2DC0-2DC62DC8-2DCE2DD0-2DD62DD8-2DDE2E2F300530063031-3035303B303C3041-3096309D-309F30A1-30FA30FC-30FF3105-312D3131-318E31A0-31BA31F0-31FF3400-4DB54E00-9FCCA000-A48CA4D0-A4FDA500-A60CA610-A61FA62AA62BA640-A66EA67F-A697A6A0-A6E5A717-A71FA722-A788A78B-A78EA790-A793A7A0-A7AAA7F8-A801A803-A805A807-A80AA80C-A822A840-A873A882-A8B3A8F2-A8F7A8FBA90A-A925A930-A946A960-A97CA984-A9B2A9CFAA00-AA28AA40-AA42AA44-AA4BAA60-AA76AA7AAA80-AAAFAAB1AAB5AAB6AAB9-AABDAAC0AAC2AADB-AADDAAE0-AAEAAAF2-AAF4AB01-AB06AB09-AB0EAB11-AB16AB20-AB26AB28-AB2EABC0-ABE2AC00-D7A3D7B0-D7C6D7CB-D7FBF900-FA6DFA70-FAD9FB00-FB06FB13-FB17FB1DFB1F-FB28FB2A-FB36FB38-FB3CFB3EFB40FB41FB43FB44FB46-FBB1FBD3-FD3DFD50-FD8FFD92-FDC7FDF0-FDFBFE70-FE74FE76-FEFCFF21-FF3AFF41-FF5AFF66-FFBEFFC2-FFC7FFCA-FFCFFFD2-FFD7FFDA-FFDC"
 
@@ -48,21 +60,43 @@ end
 local alpha = string.char(37).."a" -- alpha class, entering 
 -- percent char directly caused error
 local alphas = {}
--- the following function is not used anymore
--- local match_char = function(x) return utf_match(x,alpha) end
+
+-- test if x is a character
 local match_table = function(x, chars)
-  local chars=chars or {} 
-  return chars[x] 
-end 
+  local chars=chars or {}
+  return chars[x]
+end
 local singlechars = {} -- {a=true,i=true,z=true, v=true, u=true, o = true} 
+
+-- test if the next node after single char is a space or kern
+local function is_space_after_single(nextn)
+  if nextn == nil then return false end
+  if nextn.id == glue_id then
+    return true
+  end
+  return false
+end
+
+local function is_kern_after_single(nextn)
+  -- handle italic correction kerning before space
+  if nextn == nil then return false end
+  local after_next = nextn.next
+  if after_next == nil then return false end
+  if nextn.id == kern_id and allowed_kern_types[nextn.subtype] and after_next.id == glue_id then
+    -- when font changes the italic correction kerning is inserted, so we need to match is as true too
+    return true
+  end
+  return false
+end
+
 
 local initials = {}
 
 local getchar = function(n)
   local real_char = n.char
   -- test if char is valid utf8 value, return dummy value otherwise
-  if (real_char >= 57344 and real_char <= 63743) or (real_char >= 983040 and real_char <= 1048573) or real_char >= 1048576  then 
-    return "a" 
+  if (real_char >= 57344 and real_char <= 63743) or (real_char >= 983040 and real_char <= 1048573) or real_char >= 1048576  then
+    return "a"
   end
   return utf_char(real_char)
 end
@@ -84,15 +118,15 @@ local tex4ht = false
 -- Enable processing only for certain letters
 -- must be table in the {char = true, char2=true} form
 local set_singlechars= function(lang,c)
-  --print("Set single chars lua")
   if type(lang) == "table" then
     for _,l in pairs(lang) do
       singlechars[l] = c
     end
   else
     local lang = tonumber(lang)
-    -- for k,_ in pairs(c) do print(k) end
-    singlechars[lang] = c
+    if lang then
+      singlechars[lang] = c
+    end
   end
 end
 
@@ -103,7 +137,9 @@ local set_initials = function(lang,c)
     end
   else
     local lang = tonumber(lang)
-    initials[lang]=c
+    if lang then
+      initials[lang]=c
+    end
   end
 end
 
@@ -125,11 +161,11 @@ local debug_node = function(head,p)
   if tex4ht then
     w, head = debug_tex4ht(head,p)
   else
-    w = node.new("whatsit","pdf_literal")                          
-    w.data = "q 1 0 1 RG 1 0 1 rg 0 0 m 0 5 l 2 5 l 2 0 l b Q"           
+    w = node.new("whatsit","pdf_literal")
+    w.data = "q 1 0 1 RG 1 0 1 rg 0 0 m 0 5 l 2 5 l 2 0 l b Q"
   end
-  node.insert_after(head,head,w)                                       
-  node.insert_after(head,w,p)                                          
+  node.insert_after(head,head,w)
+  node.insert_after(head,w,p)
   -- return w
 end
 
@@ -143,13 +179,13 @@ local set_tex4ht = function()
 end
 
 local insert_penalty = function(head)
-  local p = node.new("penalty")                                           
-  p.penalty = 10000                                                       
+  local p = node.new("penalty")
+  p.penalty = 10000
   local debug = debug or false
   if debug then
     local w = debug_node(head,p)
   else
-    node.insert_after(head,head,p) 
+    node.insert_after(head,head,p)
   end
   return head
 end
@@ -168,7 +204,7 @@ end
 
 local is_alpha = function(c)
   local status = alphas[c]
-  if not status then 
+  if not status then
     status = alpha_tbl[c]
     alphas[c] = status
   end
@@ -195,7 +231,7 @@ end
 local init_buffer = ""
 local is_initial = function(c, lang)
   -- space is not initial
-  if c == " " then 
+  if c == " " then
     init_buffer = ""
     return false
   end
@@ -249,7 +285,7 @@ local is_unit = function(word)
     return true
   end
   for _, prefix in pairs(presi) do
-    s, e = string.find(word, prefix)
+    local s, e = string.find(word, prefix)
     if s == 1 then
       local unit = string.sub(word, e+1)
       if si[unit] then
@@ -272,7 +308,7 @@ end
 local predegrees = Set (require "luavlna-predegrees")
 local sufdegrees = Set (require "luavlna-sufdegrees")
 
-local function prevent_single_letter (head)                                   
+local function prevent_single_letter (head)
   local singlechars = singlechars  -- or {} 
   -- match_char matches all single letters, but this method is abbandoned
   -- in favor of using table with enabled letters. With this method, multiple
@@ -287,8 +323,9 @@ local function prevent_single_letter (head)
   local no_sufdegrees = M.no_sufdegrees
   local no_initials   = M.no_initials
   local in_math = false
+  local single_kern = false
   while head do
-    local id = head.id 
+    local id = head.id
     local nextn = head.next
     if nextn == nil then break end
     -- local skip = node.has_attribute(head, luatexbase.attributes.preventsinglestatus) 
@@ -303,7 +340,7 @@ local function prevent_single_letter (head)
         word = ""
       end
     end
-    if skip ~= 1 and not in_math  then 
+    if skip ~= 1 and not in_math  then
       if id == glue_id and allowed_space_types[head.subtype]  then
         if wasnumber then
           if word ~= "" then
@@ -312,6 +349,8 @@ local function prevent_single_letter (head)
           end
         elseif is_number(word) then
           wasnumber = true
+        elseif single_kern then
+          insert_penalty(head.prev)
         else
           word = cut_off_end_chars(word, true)
           if no_predegrees ~= true and predegrees[word] then
@@ -324,49 +363,50 @@ local function prevent_single_letter (head)
         anchor = head
         word = ""
         init = is_initial " " -- reset initials
+        single_kern = false
       elseif space==true and id == glyph_id and is_alpha(getchar(head)) then -- a letter 
         local lang = get_language(head.lang)
         local char = getchar(head)
         word = char
         init = is_initial(char,lang)
+        single_kern = false
         local s = singlechars[lang] or {} -- load singlechars for node's lang
-        --[[
-        for k, n in pairs(singlechars) do
-        for c,_ in pairs(n) do
-        --print(type(k), c)
+        if test_fn(char, s) then
+          if is_space_after_single(nextn) then    -- only if we are at a one letter word
+            head = insert_penalty(head)
+          elseif is_kern_after_single(nextn) then
+            -- if there is an italic correction after single letter, we should prevent spaces too
+            single_kern = true
+          end
         end
-        end
-        --]]
-        if test_fn(char, s) and nextn ~= nil and nextn.id == glue_id then    -- only if we are at a one letter word
-          head = insert_penalty(head)
-        end                                                                       
         space = false
         -- handle initials
         -- uppercase letter followed by period (code 46)
-      elseif no_initials~=true and init and head.id == glyph_id and head.char == period_char and nextn.id == glue_id  then 
+      elseif no_initials~=true and init and head.id == glyph_id and head.char == period_char and nextn.id == glue_id  then
         head = insert_penalty(head)
       elseif head.id == glyph_id then
         local char = getchar(head)
         word = word .. char
         init = is_initial(char, head.lang)
+        single_kern = false
         -- hlist support
       elseif head.id == hlist_id then
         prevent_single_letter(head.head)
         -- vlist support
       elseif head.id == vlist_id then
         prevent_single_letter(head.head)
-      end              
+      end
     elseif id == glyph_id and in_math then
       word = word .. getchar(head)
     end
-    head = head.next                                                            
-  end                                                                             
+    head = head.next
+  end
   -- insert spaces at the end of node list
   if wasnumber then
     handle_unit(word, anchor)
   end
   return  true
-end               
+end
 
 local hyphenate_langs = {}
 local hyphenchar = string.byte("-")
@@ -386,7 +426,7 @@ end
 
 -- detect if the current character is a hyphen
 local function is_breakable_hyphen(n)
-  return n.id == glyph_id and hyphenate_langs[n.lang] and n.char == hyphenchar and n.next 
+  return n.id == glyph_id and hyphenate_langs[n.lang] and n.char == hyphenchar and n.next
 end
 
 -- don't process hyphens if the next node is penalty
@@ -413,7 +453,6 @@ end
 -- based on ShreewatsaR's code from:
 -- https://tex.stackexchange.com/a/417883/2891
 local break_hyphens = function(head, tail)
-    local glyph_id = node.id("glyph")
     local n = head
     while n do
        local skip = node.has_attribute(n, M.preventsingleid)
